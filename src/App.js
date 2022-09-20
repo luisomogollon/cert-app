08b7d33a0110243c146c2f094e6c63b529776a5b
LouMogollonDev <madwinrar@gmail.com>
2022-09-20 11:46:32 -0400

    ultimos detalles 11:46 am

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import "./App.css";
 import imagenes from "../src/assets/imagenes";
-import { FcSignature } from "react-icons/fc";
+import { GiArchitectMask } from "react-icons/gi";
 
 function App() {
   return (
     <section className="text-gray-600   border-4 border-indigo-500 flex flex-col body-font py-6 p-2  space-y-6">
-      <a href="#_" className="w-1/4 py-4 pl-6 pr-4 md:pl-4 md:py-0">
-        <span className="p-4  text-3xl  font-black leading-none text-black select-none">
-          <span> L’Art</span>
-          <span className="text-indigo-500">.</span>
-        </span>
-      </a>
+      <header class="text-gray-600 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <span class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg class="w-16 h-16 text-indigo-400 p-2 " viewBox="0 0 15 15">
+              <GiArchitectMask />
+            </svg>
+
+            <span class="ml-3 text-xl">L’Art.</span>
+          </span>
+          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center"></nav>
+          <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Button
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-4 h-4 ml-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </header>
 
       <div className="container px-5 py-10 mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
@@ -29,25 +49,22 @@ function App() {
           <br />
 
           <p className="lg:w-2/3 flex mx-auto leading-relaxed italic text-base">
-            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-            gentrify, subway tile poke farm-to-table. Franzen you probably
-            haven't heard of them man bun deep jianbing selfies heirloom prism
-            food truck ugh squid celiac humblebrag.
+            Metal soul is a painting that represents the metal spirit of a
+            warrior facing the volcanic surface, it is a surreal chaos with dark
+            reddened palettes
           </p>
         </div>
 
         <h1 className="sm:text-4xl  text-2xl italic text-center font-semibold title-font mb-1 text-gray-900">
-          Sofia Gray Moon
+          Lucia Moon
         </h1>
 
         <section className="px-2 py-32 bg-white md:px-0">
           <div className="container items-center max-w-6xl px-8 mx-auto xl:px-5">
             <div className="flex flex-wrap items-center sm:-mx-3">
               <div className="w-full md:w-1/2 md:px-3">
-
                 <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
                   <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
-
                     <span className="text-xs text-2xl text-indigo-500 tracking-widest font-medium title-font italic mb-1">
                       Sofia Art
                     </span>
@@ -71,7 +88,6 @@ function App() {
                   <img src={imagenes.img5} alt="" />
                 </div>
               </div>
-
             </div>
           </div>
         </section>
